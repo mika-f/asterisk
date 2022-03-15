7e3db5883de6c39aed5e3c9060e5830512d2225d
Natsuneko <me@natsuneko.cat>
2022-03-15 13:31:51 +0900

    chore: add base

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use clap::Parser;
+
+mod commands;
+
+#[derive(Parser)]
+#[clap(author, about, version)]
+struct Args {
+    #[clap(subcommand)]
+    subcommand: commands::SubCommand,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args: Args = Args::parse();
+
+    let result = match args.subcommand {};
+
+    match result {
+        Ok(()) => Ok(()),
+        Err(e) => Err(Box::from(e)),
+    }
 }
